2ade7983af96b55623a67dd5cd9ff44a35b26caa
Amirali <57327052+itsamirali@users.noreply.github.com>
2023-03-15 00:24:33 +0330

    added expires time for access token

--- authorizeService.js
+++ authorizeService.js
@@ -6,16 +6,19 @@ const jwt = require("jsonwebtoken");
 
 app.use(express.json());
 
-
 app.post("/login", (req, res) => {
   const username = req.body.username;
   const user = {
     name: username,
   };
 
-  const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
-  res.json({ accessToken: accessToken });
+  const accessToken = generateAccessTokent(user);
+  const refreshToken = jwt.sign(user.process.env.REFRESH_TOKEN_SECRET);
+  res.json({ accessToken, refreshToken });
 });
 
+function generateAccessTokent(user) {
+  return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "15s" });
+}
 
 app.listen(3000);
